883986ec5e67ad55163d65d66a267f236a1f42ec
Gil Pratte <gilpratte@pingidentity.com>
2022-02-19 08:17:01 -0600

    div for current round, time, and next round

--- src/game/Clock.jsx
+++ src/game/Clock.jsx
@@ -1,9 +1,31 @@
 import './Clock.css';
+import {useState} from "react";
 
-function Clock() {
+const rounds = [
+  {'name': 'Round 1', 'bigBlind': 25,  'smallBlind': 50,  'ante': 0,   'duration': 20},
+  {'name': 'Round 2', 'bigBlind': 50,  'smallBlind': 100, 'ante': 0,   'duration': 20},
+  {'name': 'Round 3', 'bigBlind': 100, 'smallBlind': 200, 'ante': 0,   'duration': 20},
+  {'name': 'Break',   'bigBlind': 0,   'smallBlind': 0,   'ante': 0,   'duration': 5},
+  {'name': 'Round 4', 'bigBlind': 100, 'smallBlind': 200, 'ante': 100, 'duration': 20},
+  {'name': 'Round 5', 'bigBlind': 150, 'smallBlind': 300, 'ante': 150, 'duration': 20}
+];
+
+const formatTimeRemaining = (secondsRemaining) => {
+  const minutes = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+  return {'minutes': minutes, 'seconds': seconds};
+}
+
+const Clock = () => {
+  const round = rounds[0];
+  const nextRound = rounds[1];
+  const [secondsRemaining, setSecondsRemaining] = useState(round.duration * 60);
+  const timeRemaining = formatTimeRemaining(secondsRemaining);
   return (
     <div className="Clock">
-      Hello Clock
+      <div>{round.name} {timeRemaining.minutes}:{timeRemaining.seconds} {round.bigBlind} {round.smallBlind} {round.ante}</div>
+      <div>Controls</div>
+      <div>{nextRound.name} {nextRound.duration} {nextRound.bigBlind} {nextRound.smallBlind} {nextRound.ante}</div>
     </div>
   )
 }
